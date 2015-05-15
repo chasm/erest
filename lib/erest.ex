2a9285c767db5ecfd1f0d55511e573d13fff62d9
Charles F. Munat <charles.munat@gmail.com>
2015-05-15 18:03:48 +1200

    Initial dependencies.

---
+++ lib/erest.ex
@@ -0,0 +1,15 @@
+defmodule Erest do
+  import Plug.Conn
+
+  def init(options) do
+    # initialize options
+
+    options
+  end
+
+  def call(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "Goodbye, cruel world!")
+  end
+end
